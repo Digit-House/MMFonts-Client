d1379b80c1d4c917936d7f5b88577c93b58dfdad
Kaung Htet Naing <kaunghtetnaing@Kaungs-MacBook-Pro.local>
2023-06-25 16:56:48 +0700

    fix showiing multiple langage-select-box in nav-bar

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -43,7 +43,9 @@ export default function Header() {
           </div>
         </div>
         <div className="flex flex-row items-center gap-3">
-          <LangSelectBox />
+          <div className="flex lg:hidden">
+            <LangSelectBox />
+          </div>
           <LogoMenu />
         </div>
         <NavMenu
