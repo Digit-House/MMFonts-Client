b5dacd0960f355ce95faf5c1e0a1eff5dd1871a3
konyan <devkonyan@gmail.com>
2023-07-17 12:10:00 +0700

    fix site map

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -3,9 +3,9 @@ import getAllFonts from '@core/getAllFonts';
 
 export default async function sitemap(): Promise<MetadataRoute.Sitemap> {
   const baseUrl = 'https://www.mmfontshub.com';
-  const fonts = await getAllFonts();
-  const fontUrls = fonts.map((font) => ({
-    url: `${baseUrl}/fonts/${font.nameEn}`,
+  const fonts = getAllFonts();
+  const fontUrls = fonts.map((font, index) => ({
+    url: `${baseUrl}/fonts/${font}-${index}`,
     lastModified: new Date(),
   }));
 
