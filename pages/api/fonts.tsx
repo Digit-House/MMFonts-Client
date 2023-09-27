ed7189a7bae179a3506a13fac12e6d8269e9ffd8
konyan <devkonyan@gmail.com>
2023-09-27 14:20:15 +0700

    font download function

---
+++ pages/api/fonts.tsx
@@ -0,0 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+
+export default function handler(req: any, res: any) {
+  const param = req.query;
+  const fontDir = path.join(process.cwd(), 'public', 'data/fonts', param.name);
+
+  res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Cache-Control', 'max-age=180000');
+  res.status(200).json({ message: 'success' });
+
+  fs.readdir(fontDir, (err, files) => {
+    if (err) {
+      res.status(500).json({ error: 'Failed to read font directory' });
+      return;
+    }
+
+    const fontFiles = files.map((file) => ({
+      name: file,
+      url: `/data/fonts/${param.name}/${file}`,
+    }));
+    res.status(200).json(fontFiles);
+  });
+}
