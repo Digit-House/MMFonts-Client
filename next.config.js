f409cf961c313d9d9fb6fc67e1fe0fcedf76b9c6
ko nyan <konyan@kos-MacBook-Pro.local>
2025-06-04 21:29:08 +0700

    feat: remove pwa code

--- next.config.js
+++ next.config.js
@@ -25,8 +25,5 @@ const withNextIntl = require('next-intl/plugin')(
   // This is the default (also the `src` folder is supported out of the box)
   './i18n.ts'
 );
-const withPWA = require('@ducanh2912/next-pwa').default({
-  dest: 'public',
-});
 
-module.exports = withPWA(withNextIntl(nextConfig));
+module.exports = withNextIntl(nextConfig);
